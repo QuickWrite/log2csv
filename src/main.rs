2635e6884600c93ba35ef0bebbfcefaadf777830
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2025-09-17 16:57:39 +0200

    Add very basic file parsing
    As a simple file is being used to configure this project, it also has to be parsed.
    This is currently being done with a very simple algorithm.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,57 @@
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::ffi::OsString;
+
+use std::fs::File;
+
+#[derive(Debug)]
+struct L2C {
+    regex: String
+}
+
+fn get_kv_pair(base: &str) -> Option<(&str, &str)> {
+    if let Some(mid) = base.find(':') {
+        let (left, right) = base.split_at(mid);
+
+        return Some((left.trim_end(), right.trim_start_matches(':').trim()));
+    }
+
+    None
+}
+
+fn l2c_parse(l2c_reader: BufReader<File>) -> L2C {
+    let mut l2c = L2C {
+        regex: String::new()
+    };
+
+    let lines = l2c_reader.lines();
+
+    for (i, line) in lines.map_while(Result::ok).enumerate() {
+        let line = line.trim_start();
+        if line.starts_with('#') { // Skip comments
+            println!("Found comment in line {}", i);
+
+            continue;
+        }
+
+        if line.is_empty() { // Skip blank lines
+            continue;
+        }
+
+        if let Some((key, value)) = get_kv_pair(line) {
+            match key.to_lowercase().as_str() {
+                "regex" => l2c.regex = value.to_string(),
+                _ => {
+                    panic!("The key {key} is currently not known!"); // TODO: Error!
+                }
+            };
+        } else {
+            panic!("No key value pair found!"); // TODO: Error!
+        }
+    }
+
+    return l2c;
+}
 
 pub fn main() {
     let flags = xflags::parse_or_exit! {
@@ -18,5 +70,15 @@ pub fn main() {
         optional --separator separator: OsString
     };
 
+    let l2c = {
+        let l2c_file = match File::open(flags.l2c_path) {
+            Ok(file) => file,
+            Err(err) => panic!("Could not open file: {}", err),
+        };
+
+        l2c_parse(BufReader::new(l2c_file))
+    };
+
+    println!("{:#?}", l2c);
     // TODO: Rest of the program
 }
